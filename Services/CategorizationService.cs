187769bc7786d3236a8ea79b9bee8162b63c15f1
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 19:37:07 +0400

    Add CategorizationService.cs

---
+++ Services/CategorizationService.cs
@@ -0,0 +1,8 @@
+// Services/CategorizationService.cs
+using System.Diagnostics;
+public class CategorizationService {
+  public string AutoCategory(string tx){
+    // TODO: invoke Scala JAR
+    return "";
+  }
+} 
