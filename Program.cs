57fc2ff82c87e3c90e5937059daf79f649cd246e
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-05 19:37:00 +0400

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,3 @@
+// Program.cs
+using Microsoft.AspNetCore.Builder;using Microsoft.Extensions.DependencyInjection;var builder=WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();var app=builder.Build();app.MapControllers();app.Run();
